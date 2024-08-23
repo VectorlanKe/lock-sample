2532833eec84cbd2be053bdaa73914dd4f2a69af
vectorluo <vectorluo@qq.com>
2024-08-23 16:56:31 +0800

    添加乐观锁示例

---
+++ sample/1.乐观锁/CAS-Lock/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string testValue = string.Empty;
+List<Task> tasks = Enumerable.Range(0, 10).Select(p => Task.Run(UpDataValueTask)).ToList();
+Task.WaitAll(tasks.ToArray());
+Console.WriteLine("Hello, World!");
+Console.WriteLine(testValue);
+
+
+void UpDataValueTask()
+{
+    string oldValue = testValue;
+    // 计算新值
+    string newValue = Random.Shared.Next(100).ToString();
+    // CAS 更新数据
+    //比较location1和comparand是否相等，如果不相等，什么都不做；如果location1与comparand相等，则用value替换location1的值。无论比较结果相等与否，返回值都是location1中原有的值
+    //Interlocked.Increment递增，Interlocked.Decrement递减。
+    var result = Interlocked.CompareExchange(ref testValue, newValue, oldValue);
+    // 判断 CAS 是否成功
+    if (result == oldValue)
+    {
+        // CAS 成功，执行后续操作
+    }
+    Console.Out.WriteLine($"{result == oldValue} 线程id：{Thread.CurrentThread.ManagedThreadId:0000}  旧值：{oldValue}  新值：{newValue}");
+}
---
+++ sample/1.乐观锁/VersionLock/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FreeSql;
+using VersionLock.Models;
+
+IFreeSql fsql = new FreeSqlBuilder()
+    .UseConnectionString(DataType.Sqlite, @"Data Source=freedb.db")
+    .UseAutoSyncStructure(true)
+    .Build();
+string id = "1";
+await fsql.InsertOrUpdate<Counter>().SetSource(new Counter
+{
+    Id = id,
+    Count = 0,
+    Version = 0,
+}).ExecuteAffrowsAsync();
+List<Task> tasks = Enumerable.Range(0, 10).Select(p => Task.Run(CounterTask)).ToList();
+Task.WaitAll(tasks.ToArray());
+
+Console.WriteLine("Hello, World!");
+Counter resultMode = await fsql.Select<Counter>().Where(p => p.Id == id).ToOneAsync();
+Console.WriteLine(resultMode.Count);
+Console.WriteLine(resultMode.Version);
+
+
+async Task CounterTask()
+{
+    Counter model = await fsql.Select<Counter>().Where(p => p.Id == id).ToOneAsync();
+    int oldCount =model.Count;
+    model.Count = Random.Shared.Next(0, 100);
+    int result = await fsql.Update<Counter>()
+        .Set(p => p.Count, model.Count)
+        .Set(p => p.Version, model.Version + 1)
+        .Where(p => p.Id == id && p.Version == model.Version)
+        .ExecuteAffrowsAsync();
+    await Console.Out.WriteLineAsync($"{result} 线程id：{Thread.CurrentThread.ManagedThreadId:0000}  旧值：{oldCount:00}  新值：{model.Count:00}  当前版本：{model.Version:00}");
+}
