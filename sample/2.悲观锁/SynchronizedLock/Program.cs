b8296562bd5ef31f0e5a1bd500b5ab378516b307
vectorluo <vectorluo@qq.com>
2024-08-30 10:52:01 +0800

    添加描述信息

---
+++ sample/2.悲观锁/SynchronizedLock/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+int count=0;
+
+List<Task> tasks = Enumerable.Range(0, 10).Select(p => Task.Run(AddCount)).ToList();
+Task.WaitAll(tasks.ToArray());
+Console.WriteLine("Hello, World!");
+
+[MethodImpl(MethodImplOptions.Synchronized)]
+void AddCount()
+{
+    count++;
+    Console.Out.WriteLine($"线程id：{Thread.CurrentThread.ManagedThreadId:0000}  Count:{count}");
+}
